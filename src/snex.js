243ff4e68740bce3265acf9eb7d775cb4757a589
Pontus Persson <pom@skurt.com>
2017-01-05 09:42:57 -0800

    Update lib.

--- src/snex.js
+++ src/snex.js
@@ -1,35 +1,44 @@
-const Peer = require('peerjs');
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define(['Peer'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+      module.exports = factory(require('Peer'));
+  } else {
+      root.snex = factory(root.b);
+  }
+}(this, function(Peer) {
+  const SNEX_URL = 'http://snex.io';
 
-function createURL(key, id, type = 'nes') {
-  return 'http://snex.pomle.com'
-    + '/' + type + '/'
-    + '?key=' + key
-    + '&id=' + id;
-}
+  function session(key, pad) {
+    return open(key).then(({ peer, id }) => {
+      const body = new FormData();
+      body.append('key', key);
+      body.append('id', id);
+      body.append('type', pad);
 
-function open(key) {
-  return new Promise(res => {
-    const peer = new Peer({ key });
-    peer.on('open', id => res({peer, id}));
-  });
-}
-
-function connect(key, type = 'nes') {
-  return open(key)
-    .then(channel => Promise.all([
-      channel,
-      fetch(createURL(key, channel.id, type)).then(res => res.json()),
-    ])
-    .then(([channel, data]) => {
-       return {
-        channel,
-        url: data.url,
-      };
+      return fetch(SNEX_URL + '/api/v1/session', {
+        body,
+        method: 'POST',
+      })
+      .then(r => r.json())
+      .then(({id}) => {
+        return {
+          session: peer,
+          snexId: id,
+          snexURL: SNEX_URL + '/' + id,
+        }
+      });
     });
-}
+  }
 
-module.exports = {
-  connect,
-  createURL,
-  open,
-};
+  function open(key) {
+    return new Promise(res => {
+      const peer = new Peer({ key });
+      peer.on('open', id => res({peer, id}));
+    });
+  }
+
+  return {
+    session,
+  };
+}));

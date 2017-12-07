97d2c9345701ddd72122071b21d71244f19c5d85
Pontus Persson <pontus@warpinmedia.com>
2017-12-07 16:48:26 +0100

    Expand post build test.

--- tests/postbuild.js
+++ tests/postbuild.js
@@ -9,6 +9,41 @@ const snex = require('../' + package.main);
 
 describe('SNEX Lib', () => {
     it('is a module', () => {
-        expect(snex.createSession).to.be.ok();
+        expect(snex).to.be.ok();
+    });
+
+    describe('module', () => {
+        it('exports API_URL', () => {
+            expect(snex.API_URL).to.be('https://snex.io');
+        });
+
+        it('exports SIGNALING_SERVER', () => {
+            expect(snex.SIGNALING_SERVER).to.be('peer-secure.snex.io:443');
+        });
+
+        it('exports Controllers', () => {
+            expect(snex.Controllers).to.be.an(Object);
+        });
+
+        it('exports createPeer', () => {
+            expect(snex.createPeer).to.be.a(Function);
+        });
+
+        it('exports createSession()', () => {
+            expect(snex.createSession).to.be.a(Function);
+        });
+
+        it('exports joinSession()', () => {
+            expect(snex.joinSession).to.be.a(Function);
+        });
+
+        it('exports isSupported()', () => {
+            expect(snex.isSupported).to.be.a(Function);
+        });
+
+        it('exports Peer', () => {
+            expect(snex.Peer).to.be.a(Function);
+            expect(new snex.Peer()).to.be.an(Object);
+        });
     });
 });

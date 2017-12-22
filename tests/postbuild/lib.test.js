b1422f5ed7660777d23bfd65141f5528bbf10cde
Pontus Persson <pontus.alexander@gmail.com>
2017-12-22 20:25:53 +0100

    Update postbuild test.

--- tests/postbuild/lib.test.js
+++ tests/postbuild/lib.test.js
@@ -1,49 +1,48 @@
-const expect = require('expect.js');
 const sinon = require('sinon');
 const {JSDOM} = require('jsdom');
 
 global.window = new JSDOM().window;
 
-const package = require('../../package.json');
-const snex = require('../../' + package.main);
+const pkg = require('../../package.json');
+const snex = require('../../' + pkg.main);
 
 describe('SNEX Lib', () => {
     it('is a module', () => {
-        expect(snex).to.be.ok();
+        expect(snex).toBeTruthy();
     });
 
     describe('module', () => {
         it('exports API_URL', () => {
-            expect(snex.API_URL).to.be('https://snex.io');
+            expect(snex.API_URL).toBe('https://snex.io');
         });
 
         it('exports SIGNALING_SERVER', () => {
-            expect(snex.SIGNALING_SERVER).to.be('peer-secure.snex.io:443');
+            expect(snex.SIGNALING_SERVER).toBe('peer-secure.snex.io:443');
         });
 
         it('exports Controllers', () => {
-            expect(snex.Controllers).to.be.an(Object);
+            expect(snex.Controllers).toBeInstanceOf(Object);
         });
 
         it('exports createPeer', () => {
-            expect(snex.createPeer).to.be.a(Function);
+            expect(snex.createPeer).toBeInstanceOf(Function);
         });
 
         it('exports createSession()', () => {
-            expect(snex.createSession).to.be.a(Function);
+            expect(snex.createSession).toBeInstanceOf(Function);
         });
 
         it('exports joinSession()', () => {
-            expect(snex.joinSession).to.be.a(Function);
+            expect(snex.joinSession).toBeInstanceOf(Function);
         });
 
         it('exports isSupported()', () => {
-            expect(snex.isSupported).to.be.a(Function);
+            expect(snex.isSupported).toBeInstanceOf(Function);
         });
 
         it('exports Peer', () => {
-            expect(snex.Peer).to.be.a(Function);
-            expect(new snex.Peer()).to.be.an(Object);
+            expect(snex.Peer).toBeInstanceOf(Function);
+            expect(new snex.Peer()).toBeInstanceOf(Object);
         });
     });
 });

2956325b03bf62cc47d890eee99c9cd46def9949
Pontus Persson <pontus.alexander@gmail.com>
2017-12-22 22:36:42 +0100

    Decouple createAreas.

--- src/surface/area.js
+++ src/surface/area.js
@@ -1,5 +1,22 @@
 import {Vec2} from './math.js';
 
+export function createAreas(touchables) {
+  const areas = [];
+
+  return [...touchables].map(touchable => {
+    const [, type, name] = touchable.id.split('-');
+    const rect = touchable.getBoundingClientRect();
+
+    const area = new Circle(
+      rect.left + rect.width / 2,
+      rect.top + rect.height / 2,
+      rect.width * 0.5,
+      name);
+
+    return area;
+  });
+}
+
 export class Circle {
   constructor(x, y, r, id = null) {
     this.id = id;

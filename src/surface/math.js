a3cb62fa943070bf1298d5cfc8e6a76a639ff75e
Pontus Persson <pontus.alexander@gmail.com>
2017-12-24 00:35:45 +0100

    Add set, copy, equals to Vec2.

--- src/surface/math.js
+++ src/surface/math.js
@@ -1,5 +1,17 @@
 export class Vec2 {
   constructor(x, y) {
+    this.set(x, y);
+  }
+
+  copy(vec2) {
+    this.set(vec2.x, vec2.y);
+  }
+
+  equals(vec2) {
+    return this.x === vec2.x && this.y === vec2.y;
+  }
+
+  set(x, y) {
     this.x = x;
     this.y = y;
   }

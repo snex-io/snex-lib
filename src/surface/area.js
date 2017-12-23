d9e87c8ef2d3a1cacecd2171e65dd21f554f5b04
Pontus Persson <pontus.alexander@gmail.com>
2017-12-23 11:10:06 +0100

    Add Coord area.

--- src/surface/area.js
+++ src/surface/area.js
@@ -1,4 +1,5 @@
 import {Circle} from './shape.js';
+import {Vec2} from './math.js';
 import {vibe} from './vibrate.js';
 
 export class Button {
@@ -28,6 +29,29 @@ export class Button {
       this.state = state;
 
       callback(state);
+    }
+  }
+}
+
+export class Coord {
+  constructor(shape, id = null) {
+    this.shape = shape;
+    this.id = id;
+  }
+
+  onTouches (touches, callback) {
+    for (const touch of touches) {
+      if (!this.shape.intersects(touch)) {
+        continue;
+      }
+
+      if (this.shape.radius) {
+        const size = this.shape.radius;
+        const pos = new Vec2(
+          (touch.pos.x - this.shape.pos.x) / size,
+          (touch.pos.y - this.shape.pos.y) / size);
+        callback(pos);
+      }
     }
   }
 }
